e7c43790083271f8b999a4f79cd20d699a8fcc79
2QUAD <ambxone@gmail.com>
2025-01-01 19:03:45 -0300

    modelo ssh_key

--- app/models/ssh_key.rb
+++ app/models/ssh_key.rb
@@ -1,12 +1,23 @@
-# Store the public key in the database
-
+# Classe que representa uma chave SSH
 class SshKey
   def self.store(name, public_key)
-    # Store the public key in the database
+    # Armazena a chave SSH no Redis
     $redis.set("ssh_key:#{name}", public_key)
   end
 
-  def self.find(name)
-    # Find the public key in the database
+  def self.all
+    # Lista todas as chaves SSH armazenadas no Redis
+    keys = $redis.keys("ssh_key:*")
+    keys.map { |key| key.split(":").last }
+  end
+
+  def self.fetch(name)
+    # Recupera uma chave SSH pelo nome
+    $redis.get("ssh_key:#{name}")
+  end
+
+  def self.delete(name)
+    # Deleta uma chave SSH do Redis
+    $redis.del("ssh_key:#{name}")
   end
 end
